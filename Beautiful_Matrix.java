6437a54dd3bdab8f902f14bac58e3d54f0fcf329
GlocOr33 <91048475+JavaPanda30@users.noreply.github.com>
2023-09-21 23:14:08 +0530

    Add files via upload

---
+++ Beautiful_Matrix.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Beautiful_Matrix {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a[][] = new int[5][5];
+        int row = 0, col=0;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                a[i][j] = sc.nextInt();
+                if(a[i][j]==1)
+                {
+                    row=i;col=j;
+                }
+            }
+        }
+        System.out.println(Math.abs(2-row)+Math.abs(2-col));
+    }
+}
